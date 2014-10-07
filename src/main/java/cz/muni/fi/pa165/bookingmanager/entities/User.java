5d955090b27450decfb78341a17cf96c2eeeb133
Marek Stana <mstana@localhost.localdomain>
2014-10-07 17:50:32 +0200

    entity user

--- src/main/java/cz/muni/fi/pa165/bookingmanager/entities/User.java
+++ src/main/java/cz/muni/fi/pa165/bookingmanager/entities/User.java
@@ -5,10 +5,88 @@
  */
 package cz.muni.fi.pa165.bookingmanager.entities;
 
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author mstana
  */
+@Entity
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+    
+    @Column
+    private String email;
+
+    //to do: booking list
+    
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 1337;
+        hash = 23 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
     
 }
