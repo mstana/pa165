d7ec2a755d47797fb98d6ab9d3e3cb1baf20bedd
amertak <kadlec.david@gmail.com>
2014-10-07 19:44:10 +0200

    Room - DAO, Entity, Manager Reservation - Test

---
+++ src/main/java/cz/muni/fi/pa165/bookingmanager/managers/RoomManager.java
@@ -0,0 +1,38 @@
+package cz.muni.fi.pa165.bookingmanager.managers;
+
+import cz.muni.fi.pa165.bookingmanager.dao.*;
+import cz.muni.fi.pa165.bookingmanager.entities.Room;
+import java.util.List;
+
+/**
+ *
+ * @author David Kadlec
+ */
+public class RoomManager implements RoomDAO {
+
+    @Override
+    public void create(Room room) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void update(Room room) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(Room room) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Room find(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public List<Room> findAll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ src/test/java/cz/muni/fi/pa165/bookingmanager/ReservationManagerTest.java
@@ -0,0 +1,29 @@
+package cz.muni.fi.pa165.bookingmanager;
+
+import org.junit.*;
+
+/**
+ *
+ * @author David Kadlec
+ */
+public class ReservationManagerTest {
+
+    public ReservationManagerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
