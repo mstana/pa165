9d07a3a932934316e8c8fc44a6121fb9b776fcf2
opicak <pavelka@cesnet.cz>
2014-10-07 17:14:40 +0200

    added RoomManagerTest

---
+++ src/test/java/cz/muni/fi/pa165/bookingmanager/RoomManagerTest.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pa165.bookingmanager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ondrej Pavelka <106752@mail.muni.cz>
+ */
+public class RoomManagerTest {
+
+    public RoomManagerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
