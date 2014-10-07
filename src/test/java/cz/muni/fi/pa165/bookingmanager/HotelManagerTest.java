8c2959aa446e68f6cdeab9fc00fbbc1b6b7ae30f
Marek Stana <mstana@localhost.localdomain>
2014-10-07 17:08:16 +0200

    rename package for tests

--- src/test/java/cz/muni/fi/pa165/bookingmanager/HotelManagerTest.java
+++ src/test/java/cz/muni/fi/pa165/bookingmanager/HotelManagerTest.java
@@ -1,3 +1,4 @@
+package cz.muni.fi.pa165.bookingmanager;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
