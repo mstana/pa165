6a81bf8db079b1c4612847e7e9ce4c68674ea612
opicak <pavelka@cesnet.cz>
2014-11-01 22:19:56 +0100

    Added part of my part. Especially mocked and autowired test.

--- pa165_booking_manager_api/src/main/java/cz/muni/fi/pa165/bookingmanager/api/dto/ReservationTO.java
+++ pa165_booking_manager_api/src/main/java/cz/muni/fi/pa165/bookingmanager/api/dto/ReservationTO.java
@@ -1,7 +1,75 @@
 package cz.muni.fi.pa165.bookingmanager.api.dto;
 
+import java.util.Date;
+
 /**
  * @author: Ondřej Pavelka <pavelka@cesnet.cz>
  */
 public class ReservationTO {
+    private Long id;
+
+    private RoomTO room;
+
+    private Date beginDate;
+
+    private Date endDate;
+
+    private UserTO user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoomTO getRoom() {
+        return room;
+    }
+
+    public void setRoom(RoomTO room) {
+        this.room = room;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public UserTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserTO user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReservationTO that = (ReservationTO) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
--- pa165_booking_manager_service/src/main/java/cz/muni/fi/pa165/bookingmanager/services/ReservationServiceImpl.java
+++ pa165_booking_manager_service/src/main/java/cz/muni/fi/pa165/bookingmanager/services/ReservationServiceImpl.java
@@ -3,13 +3,20 @@ package cz.muni.fi.pa165.bookingmanager.services;
 
 import cz.muni.fi.pa165.bookingmanager.api.BookingDataAccessException;
 import cz.muni.fi.pa165.bookingmanager.api.dto.ReservationTO;
+import cz.muni.fi.pa165.bookingmanager.api.dto.RoomTO;
+import cz.muni.fi.pa165.bookingmanager.api.dto.UserTO;
 import cz.muni.fi.pa165.bookingmanager.api.services.ReservationService;
 import cz.muni.fi.pa165.bookingmanager.dao.ReservationDAO;
 import cz.muni.fi.pa165.bookingmanager.entities.Reservation;
+import cz.muni.fi.pa165.bookingmanager.entities.Room;
+import cz.muni.fi.pa165.bookingmanager.entities.User;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: Ondřej Pavelka <pavelka@cesnet.cz>
@@ -28,13 +35,86 @@ public class ReservationServiceImpl implements ReservationService {
         return reservationDAO;
     }
 
+    @Override
     public void create(ReservationTO reservation) {
-        Reservation reservationDAO = mapper.map(reservation,Reservation.class);
+        if (reservation == null) {
+            throw new IllegalArgumentException("Reservation cannot be null");
+        }
+        if (reservation.getId() != null) {
+            throw new IllegalArgumentException("Reservation cannot have id already set");
+        }
+        Reservation reservationDO = mapper.map(reservation, Reservation.class);
 
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        //TODO:mapping na TO
-        //Reservation transferalReservation = ReservationTOMapping.toEntity(reservation);
-        //reservationDAO.create(reservation);
+        reservationDAO.create(reservationDO);
+        reservation.setId(reservationDO.getId());
     }
+
+    @Override
+    public void update(ReservationTO reservation) {
+        if (reservation == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        if (reservation.getId() == null || reservationDAO.find(reservation.getId()) == null) {
+            throw new IllegalArgumentException("Uset does not exist");
+        }
+        Reservation reservationDO = mapper.map(reservation, Reservation.class);
+
+        reservationDAO.update(reservationDO);
+    }
+
+    @Override
+    public void delete(ReservationTO reservation) {
+        if (reservation == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        }
+        Reservation userDO = mapper.map(reservation, Reservation.class);
+
+        reservationDAO.delete(userDO);
+    }
+
+    @Override
+    public ReservationTO find(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null");
+        }
+
+        Reservation reservationDO = reservationDAO.find(id);
+        if (reservationDO == null) {
+            return null;
+        } else {
+            return mapper.map(reservationDO, ReservationTO.class);
+        }
+    }
+
+    @Override
+    public List<ReservationTO> findAll() {
+        List<Reservation> reservations = reservationDAO.findAll();
+        List<ReservationTO> reservationTOs = new ArrayList<>();
+        for (Reservation reservationDO : reservations) {
+            reservationTOs.add(mapper.map(reservationDO, ReservationTO.class));
+        }
+
+        return reservationTOs;
+    }
+
+    @Override
+    public List<ReservationTO> findAll(RoomTO room) {
+        List<Reservation> reservations = reservationDAO.findAll(mapper.map(room, Room.class));
+        List<ReservationTO> reservationTOs = new ArrayList<>();
+        for (Reservation reservationDO : reservations) {
+            reservationTOs.add(mapper.map(reservationDO, ReservationTO.class));
+        }
+
+        return reservationTOs;    }
+
+    @Override
+    public List<ReservationTO> findAll(UserTO user) {
+        List<Reservation> reservations = reservationDAO.findAll(mapper.map(user, User.class));
+        List<ReservationTO> reservationTOs = new ArrayList<>();
+        for (Reservation reservationDO : reservations) {
+            reservationTOs.add(mapper.map(reservationDO, ReservationTO.class));
+        }
+
+        return reservationTOs;    }
 }
 
