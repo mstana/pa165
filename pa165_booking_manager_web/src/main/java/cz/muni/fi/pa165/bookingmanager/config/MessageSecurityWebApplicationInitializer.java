ff18649eff28926cddf822b4d83ab89c39c74d4a
opicak <pavelka@cesnet.cz>
2015-01-03 14:59:58 +0100

    added users in xml

--- pa165_booking_manager_web/src/main/java/cz/muni/fi/pa165/bookingmanager/config/MessageSecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-/*package cz.muni.fi.pa165.bookingmanager.config;
-
-import org.springframework.security.web.context.*;
-
-public class MessageSecurityWebApplicationInitializer
-        extends AbstractSecurityWebApplicationInitializer {
-}*/
